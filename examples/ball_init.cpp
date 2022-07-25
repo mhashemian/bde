c14523cf34dc21caac5218e13ac141748ea28316
Mehdi Hashemian <mehdi@forallsecure.com>
2022-07-25 14:42:54 -0700

    Create ball_init.cpp

---
+++ examples/ball_init.cpp
@@ -0,0 +1,33 @@
+#include <ball_log.h>
+#include <ball_loggermanager.h>
+#include <ball_loggermanagerconfiguration.h>
+#include <ball_streamobserver.h>
+
+#include <bsl_iostream.h>
+#include <bsl_memory.h>
+
+#include <unistd.h>
+
+using namespace BloombergLP;
+
+BALL_LOG_SET_NAMESPACE_CATEGORY("MAIN");
+
+int main(int argc, char ** argv)
+{
+    ball::LoggerManagerConfiguration configuration;
+
+    configuration.setDefaultThresholdLevelsIfValid(ball::Severity::e_INFO);
+    ball::LoggerManagerScopedGuard lmGuard(configuration);
+
+
+    bsl::shared_ptr<ball::StreamObserver> observer =
+                            bsl::make_shared<ball::StreamObserver>(&bsl::cout);
+
+    // Register file observer with the Logger Manager.
+    ball::LoggerManager::singleton().registerObserver(observer, "default");
+
+    // Start logging.
+    BALL_LOG_INFO << "Hello, World!";
+
+    return 0;
+}
